276d6a75bea38374b3500ae7d0937f74891c85ea
catalinistrate <cistrate13@gmail.com>
2021-12-06 18:32:24 +0200

    da

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -4,21 +4,23 @@ import static com.example.CoreJavaBrushUp2.exemplu;
 
 import com.example.interfete.MethodsT;
 import com.example.interfete.PrintareFisier;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
 //    PrintareFisier printare = new PrintareFisier();
 //
 //    printare.print("dsjah");
 
 //    Chrome
-//    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-//    WebDriver driver = new ChromeDriver();
+    System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
 
 //    Firefox
 //    System.setProperty("webdriver.gecko.driver", "C:\\Users\\catalin.istrate\\Desktop\\geckodriver.exe");
@@ -43,13 +45,40 @@ public class Main {
 
 //    MethodsT d1 = new MethodsT();
 //    d1.getData()
+//
+//
+//    driver.get("http://google.com"); //hit url on the browser
+//    System.out.println(driver.getTitle()); //validate if your page title is correct
+//    Thread.sleep(3000);
+//    driver.findElement(By.id("L2AGLb")).click();
+//    Thread.sleep(3000);
+//
+//
+//    System.out.println(driver.getCurrentUrl());//validate if you are landed on correct url
+//    System.out.println(driver.getPageSource());//prints page source
+//
+//    driver.get("http://yahoo.com");
+//    Thread.sleep(3000);
+//    driver.navigate().back();
+//    Thread.sleep(2000);
+//    driver.navigate().forward();
+//    driver.quit();
+//
+
+      driver.get("http://facebook.com");
+      Thread.sleep(5000);
+      WebElement cookie_button= driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/div/div/div/div[3]/button[2]"));
+      cookie_button.click();
+      driver.findElement(By.id("email")).sendKeys("This is blabla");
+
+
 
 
   }
 
-public static String getData3(){
-  System.out.println("Hello world");
-  return "oana";
-}
+//public static String getData3(){
+//  System.out.println("Hello world");
+//  return "oana";
+//}
 
 }
